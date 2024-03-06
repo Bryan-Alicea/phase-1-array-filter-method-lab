20eb882d0b6f3b5fa488ae416c787df4c307f400
Bryan <aliceabryan1996@gmail.com>
2024-03-06 17:50:47 -0500

    all test pass

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(arrayOfDrivers, driverName) {
+    const foundDrivers = arrayOfDrivers.filter((driver) => {
+       return driver.toLowerCase() === driverName.toLowerCase()
+    })
+
+    return foundDrivers
+}
+
+function fuzzyMatch(arrayOfDrivers, input){
+    const matchingFirstLetters = arrayOfDrivers.filter((driver) => {
+        return driver.slice(0, input.length) === input
+    })
+
+    return matchingFirstLetters
+}
+
+function matchName(arrayOfDrivers, driverName) {
+    const matchingNames = arrayOfDrivers.filter((driver) => {
+        return driver.name === driverName
+    })
+
+    return matchingNames
+}
